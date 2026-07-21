a2785d05379bc76efdd2bf6b852db88431c527b7
sabrinamaimon <sabrinamihiya@gmail.com>
2026-07-22 01:14:24 +0600

    Declared expected CGPA calculator

--- modules/gpa.h
+++ modules/gpa.h
@@ -3,8 +3,14 @@
 
 #include "courseResult.h"
 
+extern const double gradeBoundaries[];
+extern const double gradePoints[];
+extern const char *gradeLetters[];
+
 double getGradePoint(CourseResult result);
 char *getLetterGrade(CourseResult result);
 double calculateGPA(CourseResult results[], int n_results);
+double calculateExpectedCGPA(double current_cgpa, double completed_credits,
+double expected_gpa, double future_credits);
 
 #endif
